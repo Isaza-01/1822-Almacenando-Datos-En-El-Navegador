8f3319ed08e3efeadb63f52ae145e366b7f7ed98
isaza-01 <jefferson.isaza01@unicatolica.edu.co>
2022-10-06 18:41:08 -0500

    almacenando datos en la web

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,10 +1,14 @@
+//creación del icono de darle check list, o tarea completada
+
 const checkComplete = () => {
   const i = document.createElement('i');
   i.classList.add('far', 'fa-check-square', 'icon');
   i.addEventListener('click', completeTask);
   return i;
 };
-// Immediately invoked function expression IIFE
+
+
+// con toggle, al dar clic busca si existe el estilo CSS  si esta lo quita y si no esta lo crea
 const completeTask = (event) => {
   const element = event.target;
   element.classList.toggle('fas');
